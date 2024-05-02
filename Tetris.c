4ddbdafbebe32fbbcfe44d0888ddc04a18462497
zxcv1249390 <zxcv1249390>
2024-05-02 11:27:07 +0800

    first commit

---
+++ Tetris.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+
+typedef enum
+{
+    EMPTY = -1,
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z
+}ShapeId;
+
+typedef enum
+{
+    RED = 41,
+    GREEN,
+    YELLOW,
+    BLUE,
+    PURPLE,
+    CYAN,
+    WHITE,
+    BLACK = 0,
+}Color;
+
+typedef struct
+{
+    ShapeId shape;
+    Color color;
+    int size;
+    char rotates[4][4][4];
+}Shape;
+
+Shape shapes[4] =
+{
+    {
+        .shape = I,
+        .color = CYAN,
+        .size = 4,
+        .rotates =
+        {
+            {
+                {0,0,0,0},
+                {1,1,1,1},
+                {0,0,0,0},
+                {0,0,0,0}
+            },
+            {
+                {0,0,1,0},
+                {0,0,1,0},
+                {0,0,1,0},
+                {0,0,1,0}
+            },
+            {
+                {0,0,0,0},
+                {0,0,0,0},
+                {1,1,1,1},
+                {0,0,0,0}
+            },
+            {
+                {0,1,0,0},
+                {0,1,0,0},
+                {0,1,0,0},
+                {0,1,0,0}
+            }
+        }
+    },
+    {
+        .shape = J,
+        .color = PURPLE,
+        .size = 3,
+        .rotates =
+        {
+            {
+                {0,1,0},
+                {0,1,0},
+                {1,1,0}
+            },
+            {
+                {1,0,0},
+                {1,1,1},
+                {0,0,0}
+            },
+            {
+                {0,1,1},
+                {0,1,0},
+                {0,1,0}
+            },
+            {
+                {0,0,0},
+                {1,1,1},
+                {0,0,1}
+            }
+        }
+    },
+    {
+        .shape = O,
+        .color = YELLOW,
+        .size = 2,
+        .rotates =
+        {
+            {
+                {1,1},
+                {1,1}
+            },
+            {
+                {1,1},
+                {1,1}
+            },
+            {
+                {1,1},
+                {1,1}
+            },
+            {
+                {1,1},
+                {1,1}
+            }
+        }
+    },
+    {
+        .shape = S,
+        .color = RED,
+        .size = 3,
+        .rotates =
+        {
+            {
+                {0,1,1},
+                {1,1,0},
+                {0,0,0}
+            },
+            {
+                {0,1,0},
+                {0,1,1},
+                {0,0,1}
+            },
+            {
+                {0,0,0},
+                {0,1,1},
+                {1,1,0}
+            },
+            {
+                {1,0,0},
+                {1,1,0},
+                {0,1,0}
+            }
+        }
+    },
+};
+
+int main()
+{
+    Color cur;
+    // 幾種方塊 (目前只有 I)
+    for (int i = 0; i < 4; i++) {
+        //印出方塊
+        //第幾個樣式
+        for (int r = 0; r < 4; r++) {
+            // 二維陣列的對應輸出
+            for (int s = 0; s < shapes[i].size; s++){
+                for (int t = 0; t < shapes[i].size; t++) {
+                    //如果是 0 就輸出白色
+                    if (shapes[i].rotates[r][s][t] == 0) {
+                        cur = WHITE;
+                    }
+                    else {
+                        cur = shapes[i].color;
+                    }
+                    printf("\033[%dm  \033[0m", cur);
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
+
